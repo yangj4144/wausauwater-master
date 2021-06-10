6a9651221d316118e28dcb3a4d0dee6b4f702d72
yangj4144 <84891650+yangj4144@users.noreply.github.com>
2021-06-09 20:56:15 -0500

    initial again

---
+++ src/JosephYangViewsWausau/Controllers/Wausau.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JosephYangViewsWausau.Controllers
+{
+    public class Wausau : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult ThingsToDo()
+        {
+
+           
+            return View();
+        }
+
+        public IActionResult AreaHotels()
+        {
+           
+            return View();
+        }
+
+        public IActionResult ClimbRibMountain()
+        {
+            return View();
+        }
+
+        public IActionResult BirdWatching()
+        {
+            return View();
+        }
+
+    }
+}
